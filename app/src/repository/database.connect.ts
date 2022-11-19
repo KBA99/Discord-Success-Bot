e182ebd3a1d1dafeaabf5bfdc4c4f6c932e47e96
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-11-19 15:41:47 +0000

    feat: export database connection to mongodb

--- app/src/repository/database.connect.ts
+++ app/src/repository/database.connect.ts
@@ -2,8 +2,8 @@
 import mongoose from 'mongoose';
 import { mongoDB } from '~/config';
 
-(async function () {
+export const connectToDatabase = async() => {
 	mongoose.connect(mongoDB.dbLocalURL, { autoCreate: true, autoIndex: true }, () =>
 		console.log('\x1b[32m%s\x1b[0m', '[Initialize][Database] MongoDB Connected 🧳')
 	);
-})();
+};
