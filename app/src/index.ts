15dd14c9dd9ad24d25714d16dce8775e6b07b9b0
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-11-19 15:44:49 +0000

    feat: add event listners for client

--- app/src/index.ts
+++ app/src/index.ts
@@ -12,6 +12,18 @@ export const client = new Client({
 	],
 });
 
+client.on('ready', () => {
+	console.log(`🤖 Bot is online and logged in as ${client.user?.tag}!`);
+});
+
+client.on('interactionCreate', (interaction: Interaction) => {});
+
+client.on('messageCreate', (message: Message) => {
+	if (!message.author.bot) {
+		message.channel.send('Sends message once');
+	}
+});
+
 (async function Initialize() {
 	client.login(discordConfig.token);
 	await connectToDatabase();
