a64fe0a4e12d7b42eda8b25f62d852c9a95e0c6f
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-11-19 15:44:12 +0000

    feat: login to discord bot

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,3 +1,8 @@
+import { Interaction, GatewayIntentBits, Message } from 'discord.js';
+import { Client } from 'discord.js';
+import { discordConfig } from '~/config';
+import { connectToDatabase } from './repository/database.connect';
+
 export const client = new Client({
 	intents: [
 		GatewayIntentBits.Guilds,
@@ -7,3 +12,7 @@ export const client = new Client({
 	],
 });
 
+(async function Initialize() {
+	client.login(discordConfig.token);
+	await connectToDatabase();
+})();
