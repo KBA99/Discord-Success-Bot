09cba05de7c037fb2248de0079644e391a819957
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-11-19 16:29:44 +0000

    feat: remove reactions if a use reacts to the reactions added to the image

--- app/src/index.ts
+++ app/src/index.ts
@@ -2,6 +2,7 @@ import { Interaction, GatewayIntentBits, Message } from 'discord.js';
 import { Client } from 'discord.js';
 import { discordConfig } from '~/config';
 import { connectToDatabase } from './repository/database.connect';
+import { timer } from './utils/helper';
 
 export const client = new Client({
 	intents: [
@@ -9,6 +10,7 @@ export const client = new Client({
 		GatewayIntentBits.GuildMembers,
 		GatewayIntentBits.MessageContent,
 		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMessageReactions,
 	],
 });
 
@@ -21,6 +23,14 @@ client.on('interactionCreate', (interaction: Interaction) => {});
 client.on('messageCreate', async (message: Message) => {
 	await addReactionsToMessageIfAttachment(message);
 });
+
+client.on('messageReactionAdd', (event, user) => {
+	if (!user.bot) {
+		console.log(event, user);
+		event.message.reactions.removeAll();
+	}
+});
+
 const addReactionsToMessageIfAttachment = async (message: Message) => {
 	// Add a check for if content type includes image
 	if (!message.author.bot) {
