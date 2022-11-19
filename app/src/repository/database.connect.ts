1c849539e8b3468747391e6f3593013c9b85e04b
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-11-19 12:28:36 +0000

    feat: add connection to mongoDB

---
+++ app/src/repository/database.connect.ts
@@ -0,0 +1,9 @@
+/* ----------------------------- MongoDB Setup ----------------------------- */
+import mongoose from 'mongoose';
+import { mongoDB } from '~/config';
+
+(async function () {
+	mongoose.connect(mongoDB.dbLocalURL, { autoCreate: true, autoIndex: true }, () =>
+		console.log('\x1b[32m%s\x1b[0m', '[Initialize][Database] MongoDB Connected 🧳')
+	);
+})();
--- config.example.ts
+++ config.example.ts
@@ -1,1 +1,4 @@
 // change the file nane to config.ts
+export const mongoDB = {
+	dbLocalURL: ''
+}
