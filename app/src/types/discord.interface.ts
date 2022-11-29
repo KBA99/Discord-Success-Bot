968ee0a985592327fd31adca5b044d6a5d562d9a
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-11-29 22:08:06 +0000

    feat: add interface for DiscordCommandConfig

--- app/src/types/discord.interface.ts
+++ app/src/types/discord.interface.ts
@@ -1,10 +1,16 @@
-import mongoose from "mongoose";
+import { SlashCommandBuilder, CommandInteraction } from 'discord.js';
+import mongoose from 'mongoose';
 
 export interface DiscordUser {
-discordId: string;
-approaved: number;
+	discordId: string;
+	approaved: number;
 	denied: number;
 	submitted: number;
+}
+
+export interface DiscordCommandConfig {
+	data: Partial<SlashCommandBuilder>;
+	execute: (interaction: CommandInteraction) => void;
 }
 
 export interface IServerSchema extends mongoose.Document {
