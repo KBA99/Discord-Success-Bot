3a2d06fc55636f0d2018a5aefa017776465072a3
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-11-30 13:44:04 +0000

    feat: add command to check for profile stats

---
+++ app/src/commands/profile.ts
@@ -0,0 +1,34 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, EmbedBuilder } from 'discord.js';
+import { findUserSuccessProfile } from '../repository/services/server.service';
+import { DiscordCommandConfig } from '../types/discord.interface';
+
+const commandDefinition = new SlashCommandBuilder()
+	.setName('profile')
+	.setDescription('Use this command to open your user success profile');
+
+const executeCommand = async (interaction: CommandInteraction) => {
+	const { user } = await findUserSuccessProfile(interaction.guild!, interaction.user.id);
+	interaction.reply({
+		embeds: [
+			new EmbedBuilder()
+				.setTitle(`Success Profile`)
+				.setColor(`#00209e`)
+				.addFields(
+					{ name: 'Approved', value: `${user?.approved}`, inline: true },
+					{ name: 'Denied', value: `${user?.denied}`, inline: true },
+					{ name: 'Submitted', value: `${user?.submitted}`, inline: true }
+				)
+				.setTimestamp(new Date()),
+		],
+		ephemeral: true,
+	});
+};
+
+// This is done to retain type hints on export
+const command: DiscordCommandConfig = {
+	data: commandDefinition,
+	execute: executeCommand,
+};
+
+export default command;
