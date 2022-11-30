656f6ecd1a6f0697f467c16d61d6a316acfa1136
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-11-30 12:07:11 +0000

    feat: add server to database if server does not exist

--- app/src/commands/register.ts
+++ app/src/commands/register.ts
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction } from 'discord.js';
-import { setSuccessChannel } from '../repository/services/server.service';
+import { addNewServerToDatabase, findServerById, setSuccessChannel } from '../repository/services/server.service';
 import { DiscordCommandConfig } from '../types/discord.interface';
 
 const commandDefinition = new SlashCommandBuilder()
@@ -8,6 +8,10 @@ const commandDefinition = new SlashCommandBuilder()
 	.setDescription('This command registers the success channel');
 
 const executeCommand = async (interaction: CommandInteraction) => {
+	const server = await findServerById(interaction.guild!)
+	if (server == null) {
+		await addNewServerToDatabase(interaction.guild!)
+	}
 	await setSuccessChannel(interaction)
 	interaction.reply('This channel has now been set as the success channel');
 };
