71d7687b5a027624c479383c735bc5badeb5783b
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-11-30 00:10:36 +0000

    feat: set success channel when using register command

--- app/src/commands/register.ts
+++ app/src/commands/register.ts
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction } from 'discord.js';
+import { setSuccessChannel } from '../repository/services/server.service';
 import { DiscordCommandConfig } from '../types/discord.interface';
 
 const commandDefinition = new SlashCommandBuilder()
@@ -7,6 +8,7 @@ const commandDefinition = new SlashCommandBuilder()
 	.setDescription('This command registers the success channel');
 
 const executeCommand = async (interaction: CommandInteraction) => {
+	await setSuccessChannel(interaction)
 	interaction.reply('This channel has now been set as the success channel');
 };
 
