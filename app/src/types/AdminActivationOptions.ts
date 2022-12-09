922cb5e570dc1a824791d0b46d9be4d8e0f73f6a
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-12-09 18:40:14 +0000

    feat: add show to admin activation actions

--- app/src/types/AdminActivationOptions.ts
+++ app/src/types/AdminActivationOptions.ts
@@ -3,13 +3,15 @@ import { User } from 'discord.js';
 export const enum AdminActivationAction {
 	Add = 'Add',
 	Remove = 'Remove',
-	Clear = 'clear',
+	Clear = 'Clear',
+	Show = 'Show',
 }
 
 export type ActivationAction =
 	| AdminActivationAction.Add
 	| AdminActivationAction.Remove
-	| AdminActivationAction.Clear;
+	| AdminActivationAction.Clear
+	| AdminActivationAction.Show;
 
 export interface AdminActivationOptions {
 	user: User | null;
