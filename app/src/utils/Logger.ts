e738c33d36ccdb9e799478ad3194798840672589
Kwabena Ankobia <Kwabena.Ankobia@gmail.com>
2022-12-24 11:46:22 +0000

    refactor: improve logger

--- app/src/utils/Logger.ts
+++ app/src/utils/Logger.ts
@@ -1,14 +1,18 @@
-import { createWriteStream } from 'fs';
+import { promises as fs } from 'fs';
+import { format } from 'util';
 
 export function logToFile(filePath: string) {
 	return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
 		const originalMethod = descriptor.value;
 
-		descriptor.value = function (...args: any[]) {
-			const result = originalMethod.apply(this, args);
+		descriptor.value = async function (...args: any[]) {
+			const result = await originalMethod.apply(this, args);
 
-			// Write the message to the text file
-			createWriteStream(filePath, { flags: 'a' }).write(`${args[0]}` + ` :: ${new Date()}\n`);
+			try {
+				await fs.appendFile(filePath, `${format(...args)} :: ${new Date()}\n`);
+			} catch (error) {
+				console.error(error);
+			}
 
 			return result;
 		};
@@ -17,12 +21,12 @@ export function logToFile(filePath: string) {
 
 export class Logger {
 	@logToFile('log.txt')
-	static info(message?: any) {
+	static async info(message?: any) {
 		console.log('\x1b[36m%s\x1b[0m', message, '\x1b[0m');
 	}
 
 	@logToFile('log.txt')
-	static warn(message: any) {
+	static async warn(message: any) {
 		console.log('\x1b[31m%s\x1b[0m', message, '\x1b[0m');
 	}
 }
